230c62ef8ffb10ac44d50d0cd14c7546f1421039
writecodex09 <writecodex09@gmail.com>
2020-06-04 02:39:24 -0700

    menambahkan struct List

--- main.go
+++ main.go
@@ -14,4 +14,17 @@ import (
 func main(){
 	//membuat koneksi ke dalam database
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1)/taptalk-api")
+	err = db.Ping()
+	if err!= nil {
+		panic("Gagal menghubungkan")
+	}
+	defer db.Close()
+
+	router := gin.Default()
+//struct ini berisikan field yang terdapat di database
+	type List struct {
+		Id	int `json: "id"`
+		Year string `json: "year"`
+		Quarter string `json: "quarter"`
+	}
 }
